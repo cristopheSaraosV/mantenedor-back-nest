692aefe78439637f89bc5cac879e8bd970b4297f
cristopher saraos <cristopher.saraos@trebol-it.com>
2025-10-03 13:41:27 -0300

    Agregar el módulo TypeOrm para la entidad User en UsersModule y exportar UsersService para su uso en otros módulos.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User } from './entities/user.entity/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])], // 👈 NECESARIO
   providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
+  exports: [UsersService], // 👈 si Auth también lo usa
 })
 export class UsersModule {}
